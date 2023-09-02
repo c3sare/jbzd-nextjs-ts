f742e918414fb188dee5cdaf8ac5f4183780c79e
Marcin Marciniuk <marcinm222@gmail.com>
2023-09-02 10:12:24 +0200

    category silence follow

--- app/actions/posts/getCategoryPagePosts.ts
+++ app/actions/posts/getCategoryPagePosts.ts
@@ -5,12 +5,15 @@ import getActionedUsersLists from "../getActionedUsersLists";
 import getActionedTagsLists from "../getActionedTagsLists";
 import parseSearchParams from "@/utils/parseSearchParams";
 import { CategoryPageProps } from "@/app/(postsPages)/components/types/CategoryPageProps";
+import { getSession } from "../getSession";
 
 export async function getCategoryPagePosts({
   params: { index, category },
   searchParams,
 }: CategoryPageProps) {
   try {
+    const session = await getSession();
+
     if (!category) return null;
 
     const categoryDB = await prisma.category.findFirst({
@@ -27,6 +30,21 @@ export async function getCategoryPagePosts({
     const { blockedUsersIds, followedUsersIds } = await getActionedUsersLists();
 
     const { blockedTagsIds, followedTagsIds } = await getActionedTagsLists();
+
+    let isBlockedCategory = false;
+
+    if (session?.user?.id) {
+      const actionedCategory = await prisma.categoryAction.findFirst({
+        where: {
+          method: "BLOCK",
+          authorId: session.user.id,
+        },
+      });
+
+      if (actionedCategory) {
+        isBlockedCategory = true;
+      }
+    }
 
     const { addTime, memContainers, title, countOnPage } =
       await parseSearchParams(searchParams);
@@ -49,32 +67,34 @@ export async function getCategoryPagePosts({
     const postsCount = await prisma.postStats.count({ where: findParams });
 
     const pagesCount = Math.ceil(postsCount / countOnPage);
+    let posts: any = [];
+    if (!isBlockedCategory) {
+      posts = await prisma.postStats.findMany({
+        where: {
+          ...findParams,
+        },
+        orderBy: {
+          addTime: "desc",
+        },
+        skip: countOnPage * (Number(index) - 1),
+        take: countOnPage,
+      });
 
-    let posts: any = await prisma.postStats.findMany({
-      where: {
-        ...findParams,
-      },
-      orderBy: {
-        addTime: "desc",
-      },
-      skip: countOnPage * (Number(index) - 1),
-      take: countOnPage,
-    });
+      posts = await addActionPostInfo(
+        posts,
+        blockedUsersIds,
+        followedUsersIds,
+        blockedTagsIds,
+        followedTagsIds
+      );
 
-    posts = await addActionPostInfo(
-      posts,
-      blockedUsersIds,
-      followedUsersIds,
-      blockedTagsIds,
-      followedTagsIds
-    );
-
-    if (!posts) {
-      return null;
+      if (!posts) {
+        return null;
+      }
     }
 
     return {
-      posts: posts as PostType[],
+      posts: posts,
       page: Number(index),
       pagesCount,
       categoryName: categoryDB.name,
