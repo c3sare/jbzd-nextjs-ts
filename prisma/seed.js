bd96afea4ce5fe911aba4ad50a1c444cf6618194
C3sare <marcinm222@gmail.com>
2023-10-17 11:56:40 +0200

    changed seeding views

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,21 +1,23 @@
 //use this to create views in mongodb database (prisma is not support views in mongodb)
 
 //command: node createviews.js
-
-const dotenv = require("dotenv");
+import dotenv from "dotenv";
 dotenv.config();
 
-const { MongoClient } = require("mongodb");
+import { MongoClient } from "mongodb";
 
 const connect = MongoClient.connect(process.env.DATABASE_URL);
 
-connect.then(async (client) => {
-  const db = client.db();
-  const collections = (await db.listCollections().toArray()).map(
-    (item) => item.name
-  );
+const load = () =>
+  connect.then(async (client) => {
+    const db = client.db();
+    const collections = (await db.listCollections().toArray()).map(
+      (collection) => collection.name
+    );
 
-  if (!collections.find((item) => item === "UserProfile"))
+    if (!collections.find((collection) => collection === "UserProfile"))
+      await db.dropCollection("UserProfile");
+
     await db.createCollection("UserProfile", {
       viewOn: "User",
       pipeline: [
@@ -233,7 +235,9 @@ connect.then(async (client) => {
       ],
     });
 
-  if (!collections.find((item) => item === "UserProfileInfo"))
+    if (!collections.find((collection) => collection === "UserProfileInfo"))
+      await db.dropCollection("UserProfileInfo");
+
     await db.createCollection("UserProfileInfo", {
       viewOn: "User",
       pipeline: [
@@ -325,7 +329,9 @@ connect.then(async (client) => {
       ],
     });
 
-  if (!collections.find((item) => item === "UserRanking"))
+    if (!collections.find((collection) => collection === "UserRanking"))
+      await db.dropCollection("UserRanking");
+
     await db.createCollection("UserRanking", {
       viewOn: "User",
       pipeline: [
@@ -393,7 +399,9 @@ connect.then(async (client) => {
       ],
     });
 
-  if (!collections.find((item) => item === "PostStats"))
+    if (!collections.find((collection) => collection === "PostStats"))
+      await db.dropCollection("PostStats");
+
     await db.createCollection("PostStats", {
       viewOn: "Post",
       pipeline: [
@@ -587,201 +595,9 @@ connect.then(async (client) => {
       ],
     });
 
-  if (!collections.find((item) => item === "PostStats"))
-    await db.createCollection("PostStats", {
-      viewOn: "Post",
-      pipeline: [
-        /////////////////////////////////
-        {
-          $lookup: {
-            from: "UserRanking",
-            localField: "authorId",
-            foreignField: "_id",
-            as: "author",
-          },
-        },
-        {
-          $set: {
-            author: {
-              $first: "$author",
-            },
-          },
-        },
-        /////////////////////////////////
-        {
-          $lookup: {
-            from: "Comment",
-            localField: "_id",
-            foreignField: "postId",
-            as: "comments",
-          },
-        },
-        {
-          $set: {
-            comments: {
-              $cond: {
-                if: {
-                  $isArray: "$comments",
-                },
-                then: {
-                  $size: "$comments",
-                },
-                else: 0,
-              },
-            },
-          },
-        },
-        ///////////////////////////////
-        {
-          $lookup: {
-            from: "Tag",
-            localField: "tagIds",
-            foreignField: "_id",
-            as: "tags",
-          },
-        },
-        ///////////////////////////////
-        {
-          $lookup: {
-            from: "Category",
-            localField: "categoryId",
-            foreignField: "_id",
-            as: "category",
-            pipeline: [
-              {
-                $lookup: {
-                  from: "Category",
-                  localField: "parentId",
-                  foreignField: "_id",
-                  as: "parent",
-                },
-              },
-              {
-                $set: {
-                  parent: {
-                    $first: "$parent",
-                  },
-                },
-              },
-            ],
-          },
-        },
-        {
-          $set: {
-            category: {
-              $first: "$category",
-            },
-          },
-        },
-        ///////////////////////////////
-        {
-          $lookup: {
-            from: "BadgePost",
-            localField: "_id",
-            foreignField: "postId",
-            as: "postBadges",
-          },
-        },
-        {
-          $set: {
-            rock: {
-              $filter: {
-                input: "$postBadges",
-                as: "badge",
-                cond: {
-                  $eq: ["$$badge.type", "ROCK"],
-                },
-              },
-            },
-            silver: {
-              $filter: {
-                input: "$postBadges",
-                as: "badge",
-                cond: {
-                  $eq: ["$$badge.type", "SILVER"],
-                },
-              },
-            },
-            gold: {
-              $filter: {
-                input: "$postBadges",
-                as: "badge",
-                cond: {
-                  $eq: ["$$badge.type", "GOLD"],
-                },
-              },
-            },
-          },
-        },
-        {
-          $unset: ["postBadges"],
-        },
-        {
-          $set: {
-            rock: {
-              $cond: {
-                if: {
-                  $isArray: "$rock",
-                },
-                then: {
-                  $size: "$rock",
-                },
-                else: 0,
-              },
-            },
-            silver: {
-              $cond: {
-                if: {
-                  $isArray: "$silver",
-                },
-                then: {
-                  $size: "$silver",
-                },
-                else: 0,
-              },
-            },
-            gold: {
-              $cond: {
-                if: {
-                  $isArray: "$gold",
-                },
-                then: {
-                  $size: "$gold",
-                },
-                else: 0,
-              },
-            },
-          },
-        },
-        /////////////////////////////
-        {
-          $lookup: {
-            from: "PostVote",
-            localField: "_id",
-            foreignField: "postId",
-            as: "pluses",
-          },
-        },
-        {
-          $set: {
-            pluses: {
-              $cond: {
-                if: {
-                  $isArray: "$pluses",
-                },
-                then: {
-                  $size: "$pluses",
-                },
-                else: 0,
-              },
-            },
-          },
-        },
-        ////////////////////////////
-      ],
-    });
+    if (!collections.find((collection) => collection === "CommentStats"))
+      await db.dropCollection("CommentStats");
 
-  if (!collections.find((item) => item === "CommentStats"))
     await db.createCollection("CommentStats", {
       viewOn: "Comment",
       pipeline: [
@@ -918,5 +734,7 @@ connect.then(async (client) => {
       ],
     });
 
-  await client.close();
-});
+    await client.close();
+  });
+
+load();
