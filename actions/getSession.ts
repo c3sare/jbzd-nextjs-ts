e4a7c2aea823ee3c7be0fa2e3b8e84db2b615725
Marcin Marciniuk <marcinm222@gmail.com>
2023-10-25 00:19:39 +0200

    get session fix

--- actions/getSession.ts
+++ actions/getSession.ts
@@ -3,11 +3,5 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export async function getSession() {
-  try {
-    const session = await getServerSession(authOptions);
-    return session;
-  } catch (err) {
-    console.log(err);
-    return null;
-  }
+  return await getServerSession(authOptions);
 }
