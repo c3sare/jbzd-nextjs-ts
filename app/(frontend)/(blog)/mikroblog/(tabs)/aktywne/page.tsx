1fb114ae47d5d36775434d08a2aa6930fbb669c4
C3sare <marcinm222@gmail.com>
2023-11-14 14:48:46 +0100

    new pages

--- app/(frontend)/(blog)/mikroblog/(tabs)/aktywne/page.tsx
+++ app/(frontend)/(blog)/mikroblog/(tabs)/aktywne/page.tsx
@@ -1,5 +1,55 @@
-const ActiveBlogsPage = () => {
-  return <div>Aktywne</div>;
+import { getSession } from "@/actions/getSession";
+import BlogPost from "../../_components/pages/BlogPost";
+import prisma from "@/libs/prismadb";
+import { notFound } from "next/navigation";
+import { getIncludePostData } from "../../_utils/getIncludePostData";
+import { transformPosts } from "../../_utils/transformPosts";
+
+type ActiveBlogsPageProps = {
+  searchParams: {
+    time?: string;
+  };
+};
+
+const ActiveBlogsPage: React.FC<ActiveBlogsPageProps> = async ({
+  searchParams,
+}) => {
+  const session = await getSession();
+
+  const selectedTime = ["24h", "12h", "6h"].includes(searchParams.time ?? "")
+    ? searchParams.time
+    : "24h";
+
+  const date = new Date();
+
+  const inHours = Number(selectedTime!.slice(0, -1));
+
+  date.setHours(date.getHours() - inHours);
+
+  if (!session?.user) return notFound();
+
+  const activeBlogs = await prisma.blogPost.findMany({
+    where: {
+      NOT: {
+        parentId: {
+          isSet: true,
+        },
+      },
+      addTime: {
+        lte: date,
+      },
+    },
+    include: {
+      ...getIncludePostData(session.user.id),
+    },
+    orderBy: {
+      addTime: "desc",
+    },
+  });
+
+  const posts = transformPosts(activeBlogs, session.user.id);
+
+  return posts.map((post) => <BlogPost key={post.id} post={post} />);
 };
 
 export default ActiveBlogsPage;
